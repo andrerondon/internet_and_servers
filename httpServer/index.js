8700df5a7aecf9eaf0aaabfc084c305ac2970b1e
andre-rondon <rondon@estudiorondon.com.br>
2020-03-02 14:42:38 -0800

    http server

---
+++ httpServer/index.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const httpServer = http.createServer((request, response) => {
+  console.log(request.path);
+  console.log(request.params);
+  console.log(request.url);
+  // request is a object that represents the HTTP request being made
+  // response is an object that contains methods we can use to create a HTTP response back
+  response.end('<h1 style="color: red;">Welcome Home</h1>');
+})
+
+const PORT = 3000;
+const DOMAIN = 'localhost';
+
+httpServer.listen(PORT, DOMAIN, () => {
+  console.log(`Server is listing at ${DOMAIN}:${PORT}`);
+})
