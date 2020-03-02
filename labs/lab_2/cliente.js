407931b2acb170e35e3b461036bb6962eb112a47
andre-rondon <rondon@estudiorondon.com.br>
2020-03-02 15:50:24 -0800

    this is lab 03

--- labs/lab_2/cliente.js
+++ labs/lab_2/cliente.js
@@ -1,1 +1,17 @@
 const net = require('net');
+
+
+const client = new net.Socket();
+
+client.on('data', (data) => {
+    console.log(data);
+});
+
+const PORT = 3800
+const LOCALHOST = '127.0.0.1';
+
+client.connect(PORT, LOCALHOST, () => {
+    console.log(" the client is connected to a server...\n");
+    client.write('Andre Arrua de Lima Rondon')
+
+})
--- labs/lab_2/server.js
+++ labs/lab_2/server.js
@@ -1,19 +1,19 @@
 const net = require('net');
 
-const server = net.creatServer( (socket) => {
-    socket.on('data', (data) =>{
-        console.log(data);
-        socket.write(data =>{
 
-        })
-        
+const server = net.createServer( (socket) => {
+    socket.setEncoding('utf8')
+    socket.on("data", (data) => {
+        console.log(data);
+        socket.write(data)
     });
 });
 
-const PORT = 4500;
+const PORT = 3800;
 const LOCALHOST = "127.0.0.1";
 
 server.listen(PORT, LOCALHOST, () => {
-    console.log("server is runnin");
+    console.log("server is running");
     
 });
+
