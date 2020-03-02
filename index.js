96a29e6b7c7f1efd3bd4844fa35921b1c575cab2
andre-rondon <rondon@estudiorondon.com.br>
2020-03-02 11:50:35 -0800

    server

---
+++ index.js
@@ -0,0 +1,7 @@
+// simple tcp server using nodeJS Net module
+const net = require('net'); // gives us a bunch of function to create a TCP function
+
+const server = net.createServer(function (socket) {
+
+})
+
